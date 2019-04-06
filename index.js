cb30c75c91937b7e9ba4773b5938fae905a92fcf
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-04-06 23:41:55 +0300

    Format according to AirBnB

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const xml2js = require('xml2js');
 const TurndownService = require('turndown');
-const turndown = new TurndownService();
 
+const turndown = new TurndownService();
 const xmlParser = new xml2js.Parser({ explicitArray: false });
 const fileContents = fs.readFileSync('wordpress_dump.xml');
 xmlParser.parseString(fileContents, (_, data) => {
-    var channel = data.rss.channel;
-    channel.item.forEach(item => {
-        if (item['wp:post_type'] == 'post') {
-            const title = item.title;
-            console.log(title);
+  const { channel } = data.rss;
+  channel.item.forEach((item) => {
+    if (item['wp:post_type'] === 'post') {
+      const { title } = item;
+      console.log(title);
 
-            const content = item['content:encoded'];
-            const content_md = turndown.turndown(content);
-            console.log(content_md);
+      const content = item['content:encoded'];
+      const contentMarkdown = turndown.turndown(content);
+      console.log(contentMarkdown);
 
-            console.log('');
-        }
-    });
+      console.log('');
+    }
+  });
 });
